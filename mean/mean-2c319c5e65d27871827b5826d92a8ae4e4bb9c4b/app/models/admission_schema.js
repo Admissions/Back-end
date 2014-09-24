e2079ba822406e52b287b31593fc1eee96e6e5ef
Patrick Gauvin <gauvin@hcs.ufl.edu>
2014-09-24 12:11:49 -0400

    Added overhauled preliminary schema for what frontend wants.

---
+++ mean/mean-2c319c5e65d27871827b5826d92a8ae4e4bb9c4b/app/models/admission_schema.js
@@ -0,0 +1,81 @@
+﻿'use strict';
+
+/**
+* Module dependencies.
+*/
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var AdmissionsSchema = new Schema({
+    personal_info: {
+        name: {
+            first: String,
+            middle: String,
+            last: String,
+            suffix: String,
+            other_names: String
+        },
+        ssn: Number,
+        ufid: Number,
+        previous_application: Boolean,
+        previous_attendance: Boolean
+        dob: {
+            month: Number,
+            day: Number,
+            year: Number
+        },
+        gender: String,
+        nationality: String,
+        ethnicity: {
+            hispanic: Boolean,
+            ethnicity: Number
+        },
+        email: String,
+        phone: {
+            personal: {
+                number: Number,
+                us: Boolean
+            },
+            work: {
+                number: Number,
+                us: Boolean
+            },
+            cell: {
+                number: Number,
+                us: Boolean
+            }
+        },
+        address: {
+            permanent: [ String, String, String ],
+            current: [ String, String, String ]
+        },
+        /* ... */
+    },
+    special_programs_application: {
+        famu_feeder: Boolean,
+        fullbright_scholar: Boolean,
+        /* ... */
+    },
+    degree_programs: {
+        primary_program: {
+            /* ... */
+        },
+        statement_of_purpose: String
+    },
+    education_and_activities: {
+        /* ... */
+    },
+    residency_affadivit: {
+        /* ... */
+    }
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
+});
+
+mongoose.model('Admissions', AdmissionsSchema);
